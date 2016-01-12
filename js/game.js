3957d4c62c701a79441b28ef262a53b2b9c16862
alex-B-Ly <abl12582@gmail.com>
2016-01-12 03:55:56 -0500

    Added verification modal.

--- js/game.js
+++ js/game.js
@@ -1,19 +1,20 @@
 document.getElementById("start").addEventListener("click", startGame);
 var started = false;
 var startButton = document.getElementById("start");
+var resultsHeader = document.getElementById('results-header');
 var finalResult = document.getElementById('final-result');
 var score = 0;
 
 var players = document.getElementsByClassName("players");
 
 function startGame() {
-    started = true;
-    startButton.text = "Start";
-    startButton.removeEventListener('click', startGame);
-    countDown();
-    for(var i=0;i<players.length;i++){
-      players[i].addEventListener('click', increaseScore);
-    }
+  started = true;
+  startButton.text = "Start";
+  startButton.removeEventListener('click', startGame);
+  countDown();
+  for(var i=0;i<players.length;i++){
+    players[i].addEventListener('click', increaseScore);
+  }
 }
 
 function countDown() {
@@ -33,6 +34,7 @@ function countDown() {
     startButton.text = "Play Again?";
     startButton.addEventListener('click', startGame);
     started = false;
+    resultsHeader.textContent = "Final Result";
     finalResult.textContent = "Your Score is " + score;
     $('#results-modal').modal('toggle');
     reInitialize();
@@ -52,8 +54,8 @@ function reInitialize() {
     players[i].removeEventListener('click', decreaseTime);
   }
   for(var i=0;i<players.length;i++){
-    players[i].addEventListener('click', increaseScore);   
-  }  
+    players[i].addEventListener('click', increaseScore);
+  }
   score = 0;
 }
 
@@ -61,5 +63,25 @@ function decreaseTime() {
   if (startButton.text >= 3) {
     startButton.text -=3;
   }
-
 }
+
+// Rules
+function ruleShow(){
+  $('#results-header').html('Game Rules');
+  $('#final-result').html("Click on as many faces as possible (only one click counts per image) in under 20 seconds. Good Luck!");
+  $('#results-modal').modal('toggle');
+}
+
+$('.rules-button').on('click', ruleShow);
+
+// Verification function
+function verification(){
+  $('.verified-img').attr({
+    src: 'img/alex.jpg',
+    alt: 'picture of developer'
+  });
+  $('.verified-by').html('Alex Ly');
+  $('#verified-modal').modal('toggle');
+}
+
+$('.verification').on('click', verification);
